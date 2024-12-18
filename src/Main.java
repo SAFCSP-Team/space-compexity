54998b5f2fcb5d3322fa0082e76b1022a2bfd680
devAlq <khalidalqdev@gmail.com>
2024-12-18 17:07:41 +0300

    taskDone

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,20 @@
-public int[] reverseArray(int[] arr) {
-    int[] reversed = new int[arr.length];
-    int j = arr.length - 1;
-    for (int i = 0; i < arr.length; i++) {
-        reversed[j] = arr[i];
-        j--;
-    }
+class Main{
+
+public static int[] reverseArray(int[] arr) {
+    int n = arr.length;
+        
+    int[] reversed = new int[n];
+
+    for (int i = 0; i < n; i++){
+    reversed[i] = arr[n - i - 1];}
+
     return reversed;
+}
+public static void main(String[] args) {
+    int[] i = {1,2,3,4,5};
+    int[] reversed = reverseArray(i);
+    for(int x=0; x<reversed.length; x++){
+        System.out.print(reversed[x] +" ");
+    }
+}
 }
