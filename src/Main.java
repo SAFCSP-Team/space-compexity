2c9de186f2dfabf1f9daff30b794485810cc257e
devAlq <khalidalqdev@gmail.com>
2024-12-18 17:48:39 +0300

    enhanced space complexity

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,21 @@
 class Main{
 
-public static int[] reverseArray(int[] arr) {
-    int n = arr.length;
-        
-    int[] reversed = new int[n];
-
-    for (int i = 0; i < n; i++){
-    reversed[i] = arr[n - i - 1];}
-
-    return reversed;
+public static void reverseArray(int[] arr) {
+    int start = 0;
+    int end  = arr.length - 1;
+    while (start < end) {
+       int temp = arr[start];
+       arr[start] = arr[end];
+       arr[end] = temp;
+       start++;
+       end --;
+    }
 }
 public static void main(String[] args) {
     int[] i = {1,2,3,4,5};
-    int[] reversed = reverseArray(i);
-    for(int x=0; x<reversed.length; x++){
-        System.out.print(reversed[x] +" ");
+    reverseArray(i);
+    for(int x=0; x<i.length; x++){
+        System.out.print(i[x] +" ");
     }
 }
 }
